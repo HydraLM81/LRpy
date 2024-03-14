a49ec832fe70bf4f004cb5ed7b0feeea793ab9ba
HydraLM81 <hydralm81@gmail.com>
2024-03-14 15:13:21 -0700

    End of school day, closing files.

---
+++ src/modules/Level_Generator/Level_Generator.py
@@ -0,0 +1,10 @@
+import asyncio
+
+"""
+Inspiration taken from Bob Nystrom (https://journal.stuffwithstuff.com/2014/12/21/rooms-and-mazes/)
+I tried to follow their ideas, but with my own twists and parameters for things. (Thanks Bob)
+"""
+
+def level_generator_function(screen):
+
+
